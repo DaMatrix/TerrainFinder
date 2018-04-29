82220a97520b4e81bb51bebbbb6444d3b234d3e8
DaMatrix <joey.rabil@gmail.com>
2018-04-29 21:36:08 +0200

    some optimizations

--- src/main/java/net/daporkchop/bedrock/Bedrock.java
+++ src/main/java/net/daporkchop/bedrock/Bedrock.java
@@ -6,7 +6,7 @@ package net.daporkchop.bedrock;
 public class Bedrock {
     public static final boolean WILDCARDS = false;
     public static final boolean DEBUG = false;
-    public static final byte[] fuL_pattern = new byte[]{
+    public static final byte[] full_pattern = new byte[]{
             1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
             1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
             1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
@@ -55,24 +55,18 @@ public class Bedrock {
 
         for (int a = 0; a < 16; ++a) {
             for (int b = 0; b < 16; ++b) {
-                seed = seed * 709490313259657689L + 1748772144486964054L;
-
-                seed = seed & ((1L << 48L) - 1L);
+                seed = (seed * 709490313259657689L + 1748772144486964054L) & 281474976710655L;
 
                 byte v = c[a * 16 + b];
 
-                A:
-                {
-                    if (WILDCARDS && v != WILDCARD) {
-                        break A;
-                    } else if (4 <= (seed >> 17) % 5) {
-                        if (c[a * 16 + b] != 1)
-                            // if a comparison fails, bail out
-                            return false;
-                    } else {
-                        if (c[a * 16 + b] != 0)
-                            return false;
-                    }
+                if (WILDCARDS && v != WILDCARD) {
+                } else if (4 <= (seed >> 17) % 5) {
+                    if (c[a * 16 + b] != 1)
+                        // if a comparison fails, bail out
+                        return false;
+                } else {
+                    if (c[a * 16 + b] != 0)
+                        return false;
                 }
 
                 seed = seed * 5985058416696778513L + -8542997297661424380L;
@@ -88,8 +82,7 @@ public class Bedrock {
 
         for (int a = 0; a < 16; a++) {
             for (int b = 0; b < 16; b++) {
-                seed = seed * 709490313259657689L + 1748772144486964054L;
-                seed = seed & ((1L << 48L) - 1L); //TODO: optimize this
+                seed = (seed * 709490313259657689L + 1748772144486964054L) & 281474976710655L;
 
                 if (4 <= (seed >> 17) % 5) {
                     chunk[a * 16 + b] = 1;
@@ -105,8 +98,8 @@ public class Bedrock {
         for (int m = 0; m <= 16 - rows; m++) {
             for (int n = 0; n <= 16 - cols; n++) {
                 match = true;
-                for (int i = 0; i < rows && match == true; i++) {
-                    for (int j = 0; j < cols && match == true; j++) {
+                for (int i = 0; match && i < rows; i++) {
+                    for (int j = 0; match && j < cols; j++) {
                         if (sub[i * cols + j] != chunk[(m + i) * 16 + (n + j)]) {
                             match = false;
                         }
@@ -125,6 +118,70 @@ public class Bedrock {
     }
 
     public static boolean super_sub_chunk_match(byte[] sub, char rows, char cols, long x, long z) {
+        byte[] bchunk = fill3x3(x, z);
+
+        boolean match;
+        for (int m = 0; m <= 48 - rows; m++) {
+            for (int n = 0; n <= 48 - cols; n++) {
+                match = true;
+                for (int i = 0; match && i < rows; i++) {
+                    for (int j = 0; match && j < cols; j++) {
+                        if (sub[i * cols + j] != bchunk[(m + i) * 48 + (n + j)]) {
+                            match = false;
+                        }
+                    }
+                }
+                if (match) {
+                    sub_match_x = m;
+                    sub_match_z = n;
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void print_chunk_pattern(long x, long z) {
+        long seed = (x * 341873128712L + z * 132897987541L) ^ 0x5DEECE66DL;
+
+        char[] buf = new char[17];
+        buf[16] = '\n';
+
+        for (int a = 0; a < 16; ++a) {
+            for (int b = 0; b < 16; ++b) {
+                seed = seed * 709490313259657689L + 1748772144486964054L;
+
+                seed = seed & ((1L << 48L) - 1L);
+
+                if (4 <= (seed >> 17) % 5) {
+                    buf[b] = '*';
+                } else {
+                    buf[b] = ' ';
+                }
+
+                seed = seed * 5985058416696778513L + -8542997297661424380L;
+            }
+            System.out.print(buf);
+        }
+    }
+
+    public static void print_super_chunk_pattern(long x, long z) {
+        byte[] bchunk = fill3x3(x, z);
+
+        char[] buf = new char[49];
+        buf[48] = '\n';
+
+        for (int i = 0; i < 48; i++) {
+            for (int j = 0; j < 48; j++) {
+                buf[j] = bchunk[i * 48 + j] == 1 ? '*' : ' ';
+            }
+            System.out.print(buf);
+        }
+    }
+
+    public static byte[] fill3x3(long x, long z) {
         byte[] bchunk = chunkPattern_super.get();
 
         for (int i = -1; i <= 1; i++) {
@@ -147,26 +204,41 @@ public class Bedrock {
             }
         }
 
-        boolean match;
-        for (int m = 0; m <= 48 - rows; m++) {
-            for (int n = 0; n <= 48 - cols; n++) {
-                match = true;
-                for (int i = 0; i < rows && match == true; i++) {
-                    for (int j = 0; j < cols && match == true; j++) {
-                        if (sub[i * cols + j] != bchunk[(m + i) * 48 + (n + j)]) {
-                            match = false;
-                        }
-                    }
-                }
-                if (match) {
-                    sub_match_x = m;
-                    sub_match_z = n;
+        return bchunk;
+    }
 
-                    return true;
+    public static void bedrock_finder_fullpattern(byte[] pattern, int id, int step, int start, int end) {
+        for (int r = start + id; r <= end; r += step) {
+            for (int i = -r; i <= r; i++) {
+                if (chunk_match(pattern, i, r)) {
+                    System.out.println("chunk: (" + i + ", " + r + "), real: (" + (i << 4) + ", " + (r << 4) + ")");
+                }
+                if (chunk_match(pattern, i, -r)) {
+                    System.out.println("chunk: (" + i + ", " + -r + "), real: (" + (i << 4) + ", " + ((-r) << 4) + ")");
                 }
             }
+            /*for(int i = -r+1; i < r; i++) {
+                if (chunk_match(pattern, r, i)) {
+                    System.out.println("chunk: (" + i + ", " + r + "), real: (" + (i << 4) + ", " + (r << 4) + ")");
+                }
+                if(chunk_match(pattern, i, -r)) {
+                    System.out.println("chunk: (" + i + ", " + -r + "), real: (" + (i << 4) + ", " + ((-r) << 4) + ")");
+                }
+            }*/
+            //TODO: figure out what the point of this is
         }
+    }
 
-        return false;
+    public static void main(String... args) {
+        final int threads = 1;
+        for (int i = 0; i < threads; i++) {
+            final int REEE = i;
+            new Thread("Bedrock scanner #" + i) {
+                @Override
+                public void run() {
+                    bedrock_finder_fullpattern(full_pattern, REEE, threads, 0, 1875000);
+                }
+            }.start();
+        }
     }
 }
---
+++ src/test/java/Print00Test.java
@@ -0,0 +1,12 @@
+import net.daporkchop.bedrock.Bedrock;
+import org.junit.Test;
+
+/**
+ * @author DaPorkchop_
+ */
+public class Print00Test {
+    @Test
+    public void test() {
+        Bedrock.print_chunk_pattern(0L, 0L);
+    }
+}
