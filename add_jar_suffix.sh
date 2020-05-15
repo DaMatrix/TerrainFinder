516852037e928a97d427bae0905ef1a626c270ed
DaPorkchop_ <daporkchop@daporkchop.net>
2020-05-15 16:30:38 +0200

    add suffix to shaded jar

--- add_jar_suffix.sh
+++ add_jar_suffix.sh
@@ -3,7 +3,8 @@
 if [ ! -d target/ ]; then echo "target/ does not exist!"; exit 1; fi
 
 cd target/
+rm original*
 for f in *.jar; do
-    mv "$f" "`echo $f $1 | perl -pe 's/^((?:[^-]*?-){2}[^-]*?)((?:-sources)?\.jar) (.+)$/$1-$3$2/g' \
+    mv "$f" "`echo $f $1 | perl -pe 's/^((?:[^-]*?-)[^-]*?)((?:-sources)?\.jar) (.+)$/$1-$3$2/g' \
                          | perl -pe 's/^(.*?) (.+)$/$1/g'`"
 done
